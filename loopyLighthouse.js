9d8908278f5189183d82319e4195d289e7252e3b
Nader Nasr <nadernasr7@gmail.com>
2021-11-08 19:48:32 -0500

    LoopLight house modulo

---
+++ loopyLighthouse.js
@@ -0,0 +1,12 @@
+for (let x = 100; x < 201; x++) {
+  // console.log(x)
+  if(x % 3 === 0){
+    console.log('Loopy')
+  } else if(x % 5 === 0){
+    console.log('Lighthouse')
+  } else if (x % 3 === 0 && x % 5 ===0 ){
+    console.log('LoopyLighthouse')
+  } else {
+    console.log(x)
+  }
+}
