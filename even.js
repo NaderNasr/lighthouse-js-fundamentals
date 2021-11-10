1260f09390d9ce3a0678820f59a1497601126c3f
Nader Nasr <nadernasr7@gmail.com>
2021-11-09 22:25:31 -0500

    simplified return

---
+++ even.js
@@ -0,0 +1,12 @@
+const isEven = function (num){
+  return num % 2 === 0
+}
+
+const twoIsEven = isEven(2)
+const threeIsEven = isEven(3)
+// console.log(twoIsEven) // true
+// console.log(threeIsEven) // false
+
+console.log(isEven(2))
+console.log(isEven(435334343))
+
