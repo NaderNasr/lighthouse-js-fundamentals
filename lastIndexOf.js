d8e428aeee6ca55d9c9f4abeb7e7495fb010a913
Nader Nasr <nadernasr7@gmail.com>
2021-11-12 13:20:29 -0500

    find the last number in the array from user input

--- lastIndexOf.js
+++ lastIndexOf.js
@@ -1,3 +1,11 @@
-function lastIndexOf (){
-  
+function lastIndexOf(arr, val) {
+  for (let i = arr.length; i >= 0; i--) {
+    if (arr[i] === val) {
+      return i
+    } 
+  }
+  return -1
 }
+
+console.log(lastIndexOf([ ], 2));
+// lastIndexOf([0, 1, 4, 1, 2], 2);
