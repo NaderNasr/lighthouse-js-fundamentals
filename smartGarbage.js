d941b399be9a237da0e2668525760be42cc6c7b7
Nader Nasr <nadernasr7@gmail.com>
2021-11-12 13:38:11 -0500

    organizing objects using if else

---
+++ smartGarbage.js
@@ -0,0 +1,12 @@
+const smartGarbage = function (trash, bins) {
+  if (trash === "waste") {
+    bins.waste += 1;
+  } else if (trash === "recycling") {
+    bins.recycling += 1;
+  } else if (trash === "compost") {
+    bins.compost += 1;
+  }
+  return bins;
+};
+
+console.log(smartGarbage("recycling", { waste: 5, recycling: 9, compost: 5 }));
