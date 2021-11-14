37ce3139bba1d951211302fd461b774f1ce866a0
Nader Nasr <nadernasr7@gmail.com>
2021-11-14 13:29:41 -0500

    refactored

--- kata/largestTwo.js
+++ kata/largestTwo.js
@@ -1,16 +1,13 @@
 const sumLargestNumbers = function (data) {
-  let arr = data;
-  arr.sort(function (a, b) {
-    let sorted = b - a;
-    return sorted;
+  data.sort(function (a, b) {
+    return b - a;
   });
-  for (let i = 0; i < arr.length; i++) {
-    // console.log(arr[i])
-    return arr[i] + arr[i + 1];
+  for (let i = 0; i < data.length; i++) {
+    return data[i] + data[i + 1];
   }
 };
 
-// console.log(sumLargestNumbers([1, 10])); //11
-// console.log(sumLargestNumbers([1, 2, 3])); //5
-// console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2])); //126
+console.log(sumLargestNumbers([1, 10])); //11
+console.log(sumLargestNumbers([1, 2, 3])); //5
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2])); //126
 console.log(sumLargestNumbers([1000, 40, 340, 60, 192, 2])); //1340
